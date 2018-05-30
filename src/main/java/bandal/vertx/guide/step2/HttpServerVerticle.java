4d377e100114826fc2134560cd7f8abdc0059966
Sumin Bae <sumin7928@naver.com>
2018-05-31 00:10:38 +0900

    base code : step2

---
+++ src/main/java/bandal/vertx/guide/step2/HttpServerVerticle.java
@@ -0,0 +1,17 @@
+package bandal.vertx.guide.step2;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+
+public class HttpServerVerticle extends AbstractVerticle {
+
+    @Override
+    public void start(Future<Void> startFuture) throws Exception {
+        super.start(startFuture);
+    }
+
+    @Override
+    public void stop(Future<Void> stopFuture) throws Exception {
+        super.stop(stopFuture);
+    }
+}
